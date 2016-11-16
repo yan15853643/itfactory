a5afad93cdd2530724f803786830b9bda251d03c
mao_siyu <mao_siyu@sina.com>
2016-11-16 16:03:46 +0800

    delete dlfc-rest

--- dlfc-rest/src/main/java/com/housecenter/common/filter/PutFilter.java
+++
@@ -1,9 +0,0 @@
-package com.housecenter.common.filter;
-
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.HttpPutFormContentFilter;
-
-@Component
-public class PutFilter extends HttpPutFormContentFilter {
-
-}
--- dlfc-rest/src/main/java/com/housecenter/service/DlfcUserService.java
+++
@@ -1,8 +0,0 @@
-package com.housecenter.service;
-
-import com.housecenter.entity.DlfcUser;
-
-public interface DlfcUserService {
-
-    public DlfcUser selectDlfcUserById(String id);
-}
